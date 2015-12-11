9218112626b44b2338bfbb8cf99a886db376f8bf
mford22392 <mford22392@gmail.com>
2015-12-11 11:28:58 -0500

    implement bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,4 +21,5 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   return app.toTree();
+  app.import("bower_components/bootstrap/dist/css/bootstrap.css")
 };
